67887e522745167708234a5684c4b493d5411cd4
keynis <keydejun@gmail.com>
2025-09-19 19:50:31 +0200

    Primera práctica

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,47 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(numPresupuesto) {
+    if (isNaN(numPresupuesto) || numPresupuesto < 0) {
+        console.log("No has introducido un número")
+        return -1;
+    }
+    else {
+        presupuesto = numPresupuesto;
+    }
+    return presupuesto;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(cadena, valorIntroducido) {
+    if(isNaN(valorIntroducido) || valorIntroducido < 0)
+        valorIntroducido = 0;
+    
+    
+        this.descripcion = cadena;
+        this.valor = valorIntroducido;
+
+        this.mostrarGasto = function() {
+            return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
+        };
+        
+        this.actualizarDescripcion = function(nuevaDescripcion) {
+            this.descripcion = nuevaDescripcion;
+        };
+
+        this.actualizarValor = function(nuevoValor) {
+            if (nuevoValor >= 0)
+                this.valor = nuevoValor;
+        }; 
+    return this;
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
---
+++ js/presentacionNavegador.js
@@ -0,0 +1,1 @@
+alert("Keyra Ruiz")
