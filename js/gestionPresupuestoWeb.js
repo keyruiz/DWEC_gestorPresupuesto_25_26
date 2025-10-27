dd71cabd3ceaa9ec0111883d41b1b9455e2811fd
keynis <keydejun@gmail.com>
2025-10-27 12:08:10 +0100

    Funcion mostrarGastoWeb de gestionPresupuestoWeb.js hecho

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -8,9 +8,42 @@ function mostrarDatoEnId(idElemento, valor) {
     }
 }
 
-function mostrarGastoWeb() {
+function mostrarGastoWeb(idElemento, gasto) {
+    let contenedor = document.getElementById(idElemento);
 
+    let divGasto = document.createElement("div");
+    divGasto.classList.add("gasto");
+
+    let divDescripcion = document.createElement("div");
+    divDescripcion.classList.add("gasto-descripcion");
+    divDescripcion.textContent = gasto.descripcion;
+    divGasto.appendChild(divDescripcion);
+
+    let divFecha = document.createElement("div");
+    divFecha.classList.add("gasto-fecha");
+    divFecha.textContent = gasto.fecha;
+    divGasto.appendChild(divFecha);
+
+    let divValor = document.createElement("div");
+    divValor.classList.add("gasto-valor");
+    divValor.textContent = gasto.valor;
+    divGasto.appendChild(divValor);
+
+    let divEtiquetas = document.createElement("div");
+    divEtiquetas.classList.add("gasto-etiquetas");
+    divEtiquetas.textContent = gasto.descripcion;
+    divGasto.appendChild(divDescripcion);
+    gasto.etiquetas.forEach(etiqueta => {
+        let spanEtiqueta = document.createElement('span');
+        spanEtiqueta.classList.add('gasto-etiquetas-etiqueta');
+        spanEtiqueta.textContent = etiqueta;
+        divEtiquetas.appendChild(spanEtiqueta);
+      });
+      divGasto.appendChild(divEtiquetas);
+    contenedor.appendChild(divGasto);
 }
+
+
 
 function mostrarGastosAgrupadosWeb() {
 
