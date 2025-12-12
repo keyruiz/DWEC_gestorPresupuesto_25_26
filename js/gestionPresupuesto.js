4d9313b93dd841615c0935321c86d9e796da4c52
keynis <keydejun@gmail.com>
2025-12-12 18:32:26 +0100

    Función transforamrListadoEtiquetas hecha

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -156,6 +156,10 @@ function agruparGastos(periodo = "mes", etiquetas = [], fechaDesde, fechaHasta =
     return resultado;
 }
 
+function transformarListadoEtiquetas(etiquetas) {
+    return etiquetas.split(/[.,:;\s]+/)
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -169,5 +173,6 @@ export   {
     calcularTotalGastos,
     calcularBalance,
     filtrarGastos,
-    agruparGastos
+    agruparGastos,
+    transformarListadoEtiquetas
 }
